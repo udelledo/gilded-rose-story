000c7843f2ac35567893245c9387651e1b1df8ec
udelledo <udelledo@gmail.com>
2022-04-20 00:04:03 +0200

    Preparation commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# A Gilded Rose story
+
+In this repository we will be doing a step-by-step tutorial on how to effectively get control of some _legacy_ code that we were tasked to maintain and develop.
+
+This journey is based on the java version of the [Gilded Rose kata](https://github.com/emilybache/GildedRose-Refactoring-Kata).
+
+The core framework used to test our code is [JUnit 5](https://junit.org/junit5/).
+
+We will be looking at how we can quickly put in place tests that verify the behaviour of our application using
+[ApprovalTests](https://github.com/approvals/ApprovalTests.Java) to quickly cover all our code, and [PiTest](https://pitest.org/) to maintain the confidence that our tests are covering all possible cases, and we have left no stones unturned. 
---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,38 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+
+    @Test
+    void foo() {
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    void verificationTest() {
+
+        Item[] items = new Item[]{
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6)};
+
+        GildedRose app = new GildedRose(items);
+        IntStream.range(0, 10).forEach(day -> app.updateQuality());
+    }
+
+}
