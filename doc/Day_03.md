39fe72d7e53ec1c2a825f77848dfb39b315f93f0
udelledo <udelledo@gmail.com>
2022-04-22 19:58:32 +0200

    Basic refactoring

--- doc/Day_03.md
+++ doc/Day_03.md
@@ -34,3 +34,101 @@ Sulfuras, Hand of Ragnaros, -1, 80
 Sulfuras, Hand of Ragnaros, 0, -1
 Sulfuras, Hand of Ragnaros, 0, 0
 ```
+
+Nonetheless the owner has asked us to update the application to include the **Clojure** item.
+
+The requirement is:
+>- "Conjured" items degrade in Quality twice as fast as normal items
+
+So let's get into the code and understand how to implement the requirement.
+
+## Getting handle of the code
+
+Many of the functionalities used now are available in most of the latest IDE.
+At the time of writing IntelliJ Idea is the tool of choice.
+
+
+### Extract variables
+First thing that pops to the eye is the amount of array access to get the item
+```
+items[i]
+```
+Let's **extract a variable**; it will help us keep track of the item processed in the loop without too much thought.
+
+### Flip equals
+
+There are lots of calls to an equals of a possibly null object that could raise a NullPointerException.
+```
+item.name.equals("Aged Brie")
+```
+This can be avoided by _flipping the .equals()_
+```
+"Aged Brie".equals(item.name)
+```
+
+### Extract methods
+
+The _.equals()_ calls found so fare are repeated around the code, and might be useful to extract them as specific methods.
+For example
+```
+"Aged Brie".equals(item.name)
+```
+could become
+```
+private boolean isAgedBrie(Item item) {
+    return "Aged Brie".equals(item.name);
+}
+```
+
+In some cases the IDE can offer to change the signature of the extracted method so that it accepts also the string used to invoke the _.equals()_
+```
+private boolean isAgedBrie(Item item, String name) {
+    return name.equals(item.name);
+}
+```
+and capture all other name checks available in the code block.
+
+As of now there's no reason to make this method so flexible, and we are happy replacing all the other places were it was used..
+
+We repeat the process for all other name check.
+
+### Merge nested if statements
+
+The previous developer had a really strange way to think of the problem and all the nested if statements show it.
+
+It's time to merge the one that don't have to be separated and let's see if we can get some order from them.
+
+For example 
+```
+if (item.quality > 0) {
+    if (!isSulfuras(item)) {
+        item.quality = item.quality - 1;
+    }
+}
+```
+
+could become
+```
+if (item.quality > 0 && !isSulfuras(item)) {
+    item.quality = item.quality - 1;
+}
+```
+
+## Checkup point
+
+I'm about to describe what we should have done at each one of the changes above, but for simplicity I'll write just here.
+
+>TEST!!!!
+
+Since we defined it as dependency we can directly execute **pitest** to make sure that our changes didn't break anything.
+
+```
+ Statistics
+================================================================================
+>> Line Coverage: 36/36 (100%)
+>> Generated 43 mutations Killed 43 (100%)
+>> Mutations with no coverage 0. Test strength 100%
+>> Ran 85 tests (1.98 tests per mutation)
+```
+
+_So far so good!_
--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,55 +8,59 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+        for (Item item : items) {
+            if (!isAgedBrie(item)
+                    && !isBackStagePass(item)) {
+                if (item.quality > 0 && !isSulfuras(item)) {
+                    item.quality = item.quality - 1;
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                    if (isBackStagePass(item)) {
+                        if (item.sellIn < 11 && item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                        if (item.sellIn < 6 && item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!isSulfuras(item)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+            if (item.sellIn < 0) {
+                if (!isAgedBrie(item)) {
+                    if (!isBackStagePass(item)) {
+                        if (item.quality > 0 && !isSulfuras(item)) {
+                            item.quality = item.quality - 1;
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = 0;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
         }
+    }
+
+    private boolean isSulfuras(Item item) {
+        return "Sulfuras, Hand of Ragnaros".equals(item.name);
+    }
+
+    private boolean isBackStagePass(Item item) {
+        return "Backstage passes to a TAFKAL80ETC concert".equals(item.name);
+    }
+
+    private boolean isAgedBrie(Item item) {
+        return "Aged Brie".equals(item.name);
     }
 }
